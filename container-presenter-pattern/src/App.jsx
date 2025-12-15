733cfb3b49744a182e9b95eaa99635a58c69dc26
abhishek-amoga <abhishek.tripathi@amoga.io>
2025-12-15 11:18:30 +0530

    initial commit

---
+++ container-presenter-pattern/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserProfileContainer from "./with-pattern/components/profile/UserProfileContainer";
+
+function App() {
+  return (
+    <div>
+      <UserProfileContainer userId={1} />
+    </div>
+  );
+}
+
+export default App;
