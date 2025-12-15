ed33245612f3199c1bb26004b0bbf67b98e1223c
abhishek-amoga <abhishek.tripathi@amoga.io>
2025-12-15 15:25:38 +0530

    done compount component pattern

--- controlled-and-uncontrolled-compo-pattern/src/App.jsx
+++ controlled-and-uncontrolled-compo-pattern/src/App.jsx
@@ -1,14 +1,30 @@
+
+import Counter from "./state-ref/components/Counter";
+import AutoFocusInput from "./state-ref/components/AutoFocusInput";
+import CounterWithRef from "./state-ref/components/CounterWithRef";
+
+import FeedbackForm from "./messy/FeedbackForm";
+
+import ControlledFeedbackForm from "./controlled/ControlledFeedbackForm";
+
+import UncontrolledFeedbackForm from "./uncontrolled/UncontrolledFeedbackForm";
+
+import UncontrolledFormNoRef from "./uncontrolled/UncontrolledFormNoRef";
+
 function App() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-xl p-4 m-4">Start Coding in React 19</h1>
-      <p className="bg-gray-300 p-2 rounded-sm text-3xl">
-        <a 
-          href="https://www.youtube.com/watch?v=hiiGUjEkzbM&list=PLIJrr73KDmRw-T8bdJn3XxVMbH-zlooKb" 
-          target="_blank"
-          className="underline mx-1">Learn React 19 With Code</a>
-        <span>▶️</span>  
-      </p>
+      <Counter />
+      <AutoFocusInput />
+      <CounterWithRef />
+
+      <FeedbackForm />
+
+      <ControlledFeedbackForm />
+
+      <UncontrolledFeedbackForm />
+
+      <UncontrolledFormNoRef />
     </div>
   );
 }
